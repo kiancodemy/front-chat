922acde371e3e377c5121a7dca8527404de120b5
kian kheirani <kiankheirani@gmail.com>
2024-12-05 19:08:16 +0330

    'initial'

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+export default function App() {
+  return (
+    <div className="w-full min-h-screen flex flex-col">
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/"></Route>
+      </Routes>
+    </div>
+  );
+}
